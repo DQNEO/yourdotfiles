dda913e125d19a8e862ac335ceea8c64f182a9de
DQNEO <dqneoo@gmail.com>
2014-10-18 18:01:16 +0900

    indent

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-  var list = [];
-  names.pop();
-  $(function(){
-    $.each(names, function(i,name){
-       var url = 'http://github.com/' + name + '/dotfiles';
-        var li =  $('<li>').append($('<a>').attr('href', url).attr('target','_blank').text(name)); 
-       list.push(  li  );
-    });
-      
-    $.each(list, function(i, li){
-      $('#main ul').append(li);
-      });
+var list = [];
+names.pop();
+$(function(){
+  $.each(names, function(i,name){
+    var url = 'http://github.com/' + name + '/dotfiles';
+    var li =  $('<li>').append($('<a>').attr('href', url).attr('target','_blank').text(name));
+    list.push(  li  );
   });
+  $.each(list, function(i, li){
+    $('#main ul').append(li);
+  });
+});
